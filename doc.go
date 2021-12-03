6361df8930d33b722ab3e6d9dc831f8b1b991d7b
sfeldma <sfeldma@gmail.com>
2021-12-02 23:25:17 -0800

    add doc.go

---
+++ doc.go
@@ -0,0 +1,44 @@
+// Copyright 2021 Scott Feldman (sfeldma@gmail.com). All rights reserved.
+// Use of this source code is governed by a GPLv3 license that can be found in
+// the LICENSE file.
+
+/*
+Merle is a "shortest stack" IoT framework.  The stack spans hardware access at
+the bottom to html/javascript presentation at the top.
+
+Overview
+
+A device in Merle is the "Thing" in IoT.  A device is modeled with an IModel
+interface.
+
+	type IModel interface {
+		Init(*Device) error
+		Run()
+		Receive(*Packet)
+		HomePage(http.ResponseWriter, *http.Request)
+	}
+
+An implementation of IModel is the "device driver" for the device.  Init()
+initializes the device hardware and structures.  Run() runs the main loop for
+the device.  The device stops when Run() exits.  Receive() handles incoming
+Packets, where a Packet is a container for a []byte message payload.  The
+IModel implemenation defines the contents of the messages and their symantics.
+HomePage() is the device's html home page.
+
+Use merle.NewDevice to create a new device from an IModel.
+
+	model := ... // an IModel
+	device := merle.NewDevice(model, ...)
+
+Use Device.Run to start running the device.  Device.Run will 1) init and run
+the IModel; 2) start an optional web server to serve up the device's home page;
+and 3) optionally, create an SSH tunnel to a hub.
+
+	device.Run(...)
+
+The hub is an aggregator of devices.  Zero of more devices connect to the hub
+over SSH tunnels, one SSH tunnel per device.  The hub runs it's own web server
+and serves up the devices' home pages.
+
+*/
+package merle
