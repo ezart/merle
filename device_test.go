5c028fd919fcc56686b396be3af1ec726c57e361
Scott Feldman <sfeldma@gmail.com>
2021-12-07 05:24:10 +0000

    work on testing

--- device_test.go
+++ device_test.go
@@ -1,9 +1,43 @@
 package merle
 
 import (
+	"net/http"
 	"testing"
+	"time"
 )
 
+type model struct {
+}
+
+func (m *model) Init(d *Device) error {
+	return nil
+}
+
+func (m *model) Run() {
+}
+
+func (m *model) Receive(p *Packet) {
+}
+
+func HomePage(w http.ResponseWrite, r *http.Request) {
+}
+
 func TestSomething(t *testing.T) {
-	t.Errorf("Need to write device tests...")
+	var m model
+	var inHub = false
+	var id = "HS30-01132"  // sorry if that's your car
+	var model = "240z"
+	var name = "Fairlady"
+	var status = "online"
+	var startupTime = time.Now()
+
+	d = NewDevice(&m, inHub, id, model, name, status, startupTime)
+	if d == nil {
+		t.Errorf("Create new device failed")
+	}
+
+	err := d.Run("", "", "", "")
+	if err != nil {
+		t.Errorf("Run failed: %s", err)
+	}
 }
