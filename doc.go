7f5a1d142e9d05e7acce5a7f11af94c71ba23a6e
Scott Feldman <sfeldma@gmail.com>
2021-12-10 09:48:08 -0800

    separate READMD.md from doc.go

--- doc.go
+++ doc.go
@@ -3,30 +3,6 @@
 // in the LICENSE file.
 
 /*
-Merle is a "shortest stack" IoT framework.  The stack spans hardware access at
-the bottom to html presentation at the top.  Merle uses websockets for
-messaging.
-
-Status
-
-Alpha quatility of code here...
-
-Installation
-
-Merle comprises two packages: core and devices.  Install the core package from here:
-
-	go get github.com/scottfeldman/merle
-
-The devices package contains a library of devices.  Install the merle devices
-package from here:
-
-	go get github.com/scottfeldman/merle_devices
-
-Quickstart
-
-See Quickstart in github.com/scottfeldman/merle_devices for building sample
-devices for common IoT device hardware configurations.
-
 Overview
 
 A device in Merle is the "Thing" in IoT.  A device is modeled with an IModel
--- go.mod
+++ go.mod
@@ -20,9 +20,14 @@ require (
 	github.com/posener/goaction v0.1.0 // indirect
 	github.com/posener/goreadme v1.4.2 // indirect
 	github.com/posener/script v1.1.5 // indirect
+	github.com/yuin/goldmark v1.4.1 // indirect
+	golang.org/x/mod v0.5.1 // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
-	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.8 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/appengine v1.6.5 // indirect
 )
 
