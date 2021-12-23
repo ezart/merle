30da185505399827f5ac1cf5848e1395882140a1
sfeldma <sfeldma@gmail.com>
2021-12-23 01:14:16 -0800

    raspi_blink work

--- thing.go
+++ thing.go
@@ -4,35 +4,84 @@ import (
 	"log"
 	"time"
 	"net"
+	"net/http"
+	"encoding/json"
 )
 
 type Thing struct {
 	Init func() error
 	Run func()
+	Home func(w http.ResponseWriter, r *http.Request)
 
 	Status        string
 	Id            string
 	Model         string
 	Name          string
 	StartupTime   time.Time
+
+	handlers      map[string]func(*Packet)
 }
 
-func (t *Thing) prefix() string {
+func (t *Thing) logPrefix() string {
 	return "["+t.Id+","+t.Model+","+t.Name+"]"
 }
 
 func (t *Thing) Start() {
 	if t.Init != nil {
-		log.Println(t.prefix(), "Init...")
+		log.Println(t.logPrefix(), "Init...")
 		if err := t.Init(); err != nil {
-			log.Fatalln(t.prefix(), "Init failed:", err)
+			log.Fatalln(t.logPrefix(), "Init failed:", err)
 		}
 	}
 	if t.Run != nil {
-		log.Println(t.prefix(), "Run...")
+		log.Println(t.logPrefix(), "Run...")
 		t.Run()
 	}
-	log.Fatalln(t.prefix(), "Run() didn't run forever")
+	log.Fatalln(t.logPrefix(), "Run() didn't run forever")
+}
+
+func (t *Thing) Sink(p *Packet) {
+}
+
+func (t *Thing) Broadcast(p *Packet) {
+}
+
+func (t *Thing) NewPacket(msg interface {}) *Packet {
+	var p Packet
+	p.Msg, _ = json.Marshal(&msg)
+	return &p
+}
+
+func (t *Thing) AddHandler(msgType string, f func(*Packet)) {
+	if t.handlers == nil {
+		t.handlers = make(map[string]func(*Packet))
+	}
+	t.handlers[msgType] = f
+}
+
+type homeParams_ struct {
+	Scheme string
+	Host   string
+	Status string
+	Id     string
+	Model  string
+	Name   string
+}
+
+func (t *Thing) HomeParams(r *http.Request) *homeParams_ {
+	scheme := "wss:\\"
+	if r.TLS == nil {
+		scheme = "ws:\\"
+	}
+
+	return &homeParams_{
+		Scheme: scheme,
+		Host:   r.Host,
+		Status: t.Status,
+		Id:     t.Id,
+		Model:  t.Model,
+		Name:   t.Name,
+	}
 }
 
 // DefaultId returns a default ID based on the device's MAC address
---
+++ things/raspi_blink/raspi_blink.go
@@ -0,0 +1,102 @@
+// Copyright 2021 Scott Feldman (sfeldma@gmail.com). All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
+package raspi_blink
+
+import (
+	"html/template"
+	"github.com/scottfeldman/merle"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/raspi"
+	"time"
+	"net/http"
+)
+
+var templ *template.Template
+
+func init() {
+	templ = template.Must(template.ParseFiles("web/templates/raspi_blink.html"))
+}
+
+type blinker struct {
+	merle.Thing
+	adaptor *raspi.Adaptor
+	led *gpio.LedDriver
+	paused bool
+}
+
+func (b *blinker) init() error {
+	b.adaptor = raspi.NewAdaptor()
+	b.adaptor.Connect()
+
+	b.led = gpio.NewLedDriver(b.adaptor, "7")
+	b.led.Start()
+
+	return nil
+}
+
+type msgState struct {
+	Type string
+	State bool
+}
+
+func (b *blinker) sendState() {
+	var msg = msgState{
+		Type:  "state",
+		State: b.led.State(),
+	}
+	b.Broadcast(b.NewPacket(&msg))
+}
+
+func (b *blinker) run() {
+	ticker := time.NewTicker(time.Second)
+
+	b.sendState()
+
+	for {
+		select {
+		case <-ticker.C:
+			if !b.paused {
+				b.led.Toggle()
+				b.sendState()
+			}
+		}
+	}
+}
+
+func (b *blinker) home(w http.ResponseWriter, r *http.Request) {
+	templ.Execute(w, b.HomeParams(r))
+}
+
+func (b *blinker) pause(p *merle.Packet) {
+	b.paused = true
+	b.Sink(p)
+	b.Broadcast(p)
+}
+
+func (b *blinker) resume(p *merle.Packet) {
+	b.paused = false
+	b.Sink(p)
+	b.Broadcast(p)
+}
+
+func NewThing(name string) *merle.Thing {
+	b := blinker{}
+
+	b.Status = "online"
+	b.Id = merle.DefaultId_()
+	b.Model = "blinker"
+	b.Name = name
+	b.StartupTime = time.Now()
+
+	b.Init = b.init
+	b.Run = b.run
+	b.Home = b.home
+
+	b.AddHandler("pause", b.pause)
+	b.AddHandler("resume", b.resume)
+	b.AddHandler("state", b.Broadcast)
+
+	return &b.Thing
+}
