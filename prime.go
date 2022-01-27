511caaf5af99e9326144ca8de9006d35d7fdc1ec
sfeldma <sfeldma@gmail.com>
2022-01-27 13:07:56 -0800

    add copyrights to *.go files

--- prime.go
+++ prime.go
@@ -1,3 +1,7 @@
+// Copyright 2021-2022 Scott Feldman (sfeldma@gmail.com). All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
 package merle
 
 import (
