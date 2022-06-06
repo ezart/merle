3edaa198c9f600e834305c26f3a67738f075001c
Scott Feldman <sfeldma@gmail.com>
2022-06-06 14:26:17 -0700

    Delete doc.go

--- doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2021-2022 Scott Feldman (sfeldma@gmail.com). All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be found
-// in the LICENSE file.
-
-/*
-Merle is a framework for building secure web-apps for your IoT project.
- */
-package merle
