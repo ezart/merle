db38fe241d091cc2349ceab3952540507fd1d181
Scott Feldman <sfeldma@gmail.com>
2022-06-14 22:06:07 +0100

    can: add example

---
+++ examples/can/can.go
@@ -0,0 +1,76 @@
+// file: examples/can/can.go
+
+package main
+
+import (
+	"flag"
+	"github.com/go-daq/canbus"
+	"github.com/merliot/merle"
+	"log"
+)
+
+type can struct {
+}
+
+type canMsg struct {
+	Msg string
+	Id  uint32
+	Data []byte
+}
+
+func (c *can) run(p *merle.Packet) {
+	sock, err := canbus.New()
+	if err != nil {
+		log.Println("Creating CAN bus failed:", err)
+		return
+	}
+
+	iface := "can0"
+
+	err = sock.Bind(iface)
+	if err != nil {
+		log.Printf("Binding to %s failed: %s", iface, err)
+		return
+	}
+
+	msg := &canMsg{Msg: "CAN"}
+
+	for {
+		msg.Id, msg.Data, err = sock.Recv()
+		if err != nil {
+			log.Println("Error reading CAN socket:", err)
+			return
+		}
+		p.Marshal(&msg).Broadcast()
+	}
+}
+
+func (c *can) getState(p *merle.Packet) {
+	msg := struct{ Msg string }{Msg: "ReplyState"}
+	p.Marshal(&msg).Reply()
+}
+
+func (c *can) Subscribers() merle.Subscribers {
+	return merle.Subscribers{
+		merle.CmdRun:     c.run,
+		merle.GetState:   c.getState,
+		merle.ReplyState: nil,
+	}
+}
+
+func main() {
+	thing := merle.NewThing(&can{})
+
+	thing.Cfg.Model = "can"
+	thing.Cfg.Name = "can0"
+	thing.Cfg.User = "merle"
+
+	flag.StringVar(&thing.Cfg.MotherHost, "rhost", "", "Remote host")
+	flag.StringVar(&thing.Cfg.MotherUser, "ruser", "merle", "Remote user")
+	flag.BoolVar(&thing.Cfg.IsPrime, "prime", false, "Run as Thing Prime")
+	flag.UintVar(&thing.Cfg.PortPublicTLS, "TLS", 0, "TLS port")
+
+	flag.Parse()
+
+	log.Fatalln(thing.Run())
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,9 @@
 module github.com/merliot/merle
 
-go 1.17
+go 1.15
 
 require (
+	github.com/go-daq/canbus v0.0.0-20161123191156-079be98fdbd7
 	github.com/gorilla/mux v1.8.0
 	github.com/gorilla/websocket v1.5.0
 	github.com/msteinert/pam v1.0.0
@@ -10,16 +11,4 @@ require (
 	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
 	gobot.io/x/gobot v1.16.0
 	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
-)
-
-require (
-	github.com/gofrs/uuid v4.0.0+incompatible // indirect
-	github.com/hashicorp/errwrap v1.0.0 // indirect
-	github.com/hashicorp/go-multierror v1.0.0 // indirect
-	github.com/sigurn/crc8 v0.0.0-20160107002456-e55481d6f45c // indirect
-	github.com/sigurn/utils v0.0.0-20190728110027-e1fefb11a144 // indirect
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
-	golang.org/x/text v0.3.6 // indirect
-	periph.io/x/periph v3.6.2+incompatible // indirect
 )
